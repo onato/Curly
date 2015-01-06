8ad068c02f8e7c97b1ac227690775c4e32b6dad2
Stephen Williams <stephen@onato.com>
2015-01-06 22:32:40 +0100

    Add python code generator.

---
+++ code/generators/Python-Requests.js
@@ -0,0 +1,41 @@
+function generateCode(service) {
+
+    var url = service.url;
+    if (["GET", "HEAD", "DELETE"]) {
+        url += "?" + service.data;
+    };
+
+	var code = "# Install the \"Requests\" library\n";
+	code += "# pip install requests\n\n";
+    code += "import requests\n\n" 
+    code += "try:\n";
+    code += "  r = requests." + service.method.toLowerCase() + "(\n"
+	code += "    url = '"+url+"',\n";
+    code += "    params = " + formatData(service.data) + ",\n";
+
+    code += "    headers = {\n";
+    for(key in service.headers) {
+        var header = service.headers[key];
+        code += "      '"+header.type+"': '"+header.value+"',\n";
+    }
+    code += "    },\n";
+    code += "  )\n";
+
+    code += "  print('HTTP Status Code : {status_code}'.format(status_code=r.status_code))\n";
+    code += "  print('Response : {content}'.format(content=r.content))\n";
+    code += "except requests.exceptions.RequestException as e:\n";
+    code += "  print e\n";
+
+	return code;
+}
+
+function formatData(data){
+    var returnDict = {};
+    var elements = data.split("&");
+    for(key in elements) {
+        var paramParts = elements[key].split("=");
+        returnDict[paramParts[0]] = paramParts[1];
+    };
+
+    return JSON.stringify(returnDict);
+}
